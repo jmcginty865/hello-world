a193816c141c8888e7064026427954620e49f60b
Jennifer <jmcgnty23@gmail.com>
2016-09-30 10:51:13 -0400

    Working on making it display
    Got most of the actual information to work just can't get it to display Hello world.

---
+++ hello_world/Display.java
@@ -0,0 +1,10 @@
+package mcginty.jennifer.hello_world;
+
+/**
+ * Created by nosec on 9/30/2016.
+ */
+public class Display {
+    public void printString(String output){
+        System.out.println(output);
+    }
+}
---
+++ hello_world/Main.java
@@ -0,0 +1,9 @@
+package mcginty.jennifer.hello_world;
+
+public class Main {
+
+    public static void main(String[] args) {
+	HelloWorld helloWorld = new HelloWorld();
+
+    }
+    }
